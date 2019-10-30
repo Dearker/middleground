2baa9dde7c2360c2b8de0177e4a88ba662988962
Dearker <1823268262@qq.com>
2019-10-30 23:08:42 +0800

    first commit

---
+++ hanyi-job/src/main/java/com/hanyi/job/JobApplication.java
@@ -0,0 +1,20 @@
+package com.hanyi.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: middleground com.hanyi.job JobApplication
+ * @Author: weiwenchang
+ * @Description: java类作用描述
+ * @CreateDate: 2019-10-30 22:18
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class JobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class,args);
+    }
+
+}
---
+++ hanyi-job/src/main/java/com/hanyi/job/jobhandler/ShardingJobHandler.java
@@ -0,0 +1,38 @@
+package com.hanyi.job.jobhandler;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.IJobHandler;
+import com.xxl.job.core.handler.annotation.JobHandler;
+import com.xxl.job.core.log.XxlJobLogger;
+import com.xxl.job.core.util.ShardingUtil;
+import org.springframework.stereotype.Service;
+
+/**
+ * 分片广播任务
+ *
+ * @author hanyi 2019-10-30
+ */
+@JobHandler(value="shardingJobHandler")
+@Service
+public class ShardingJobHandler extends IJobHandler {
+
+	@Override
+	public ReturnT<String> execute(String param) {
+
+		// 分片参数
+		ShardingUtil.ShardingVO shardingVO = ShardingUtil.getShardingVo();
+		XxlJobLogger.log("分片参数：当前分片序号 = {}, 总分片数 = {}", shardingVO.getIndex(), shardingVO.getTotal());
+
+		// 业务逻辑
+		for (int i = 0; i < shardingVO.getTotal(); i++) {
+			if (i == shardingVO.getIndex()) {
+				XxlJobLogger.log("第 {} 片, 命中分片开始处理", i);
+			} else {
+				XxlJobLogger.log("第 {} 片, 忽略", i);
+			}
+		}
+
+		return SUCCESS;
+	}
+
+}
