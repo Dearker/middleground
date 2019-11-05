fd3844bfd62bf7b85fbab3a99060ea405d6cf081
Dearker <1823268262@qq.com>
2019-11-05 23:03:25 +0800

    first commit

--- hanyi-admin/src/main/java/com/hanyi/admin/AdminBootstrap.java
+++ hanyi-admin/src/main/java/com/hanyi/admin/AdminBootstrap.java
@@ -3,7 +3,6 @@ package com.hanyi.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -13,12 +12,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @create 2019-10-24
  */
 @EnableDiscoveryClient
-@EnableCircuitBreaker
 @SpringBootApplication
 @EnableAdminServer
 public class AdminBootstrap {
     public static void main(String[] args) {
-        //new SpringApplicationBuilder(AdminBootstrap.class).run(args);
         SpringApplication.run(AdminBootstrap.class,args);
     }
 }
