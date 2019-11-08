cbbc8feb8ebda271cc4bd48c279bb6d97bf6afdb
hanyi <m15827530340@163.com>
2019-11-08 08:58:06 +0800

    second commit

---
+++ hanyi-admin/src/main/java/com/hanyi/admin/AdminServerApplication.java
@@ -0,0 +1,21 @@
+package com.hanyi.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author hanyi
+ * @create 2019-10-24
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class,args);
+    }
+}
