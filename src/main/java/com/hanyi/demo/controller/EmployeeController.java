7168975e992b7a6877d5cd8fbd5923fbf0994dfe
Dearker <1823268262@qq.com>
2019-11-09 09:11:06 +0800

    first commit

---
+++ src/main/java/com/hanyi/demo/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.hanyi.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author hanyi
+ * @since 2019-11-09
+ */
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+}
+
---
+++ src/main/java/com/hanyi/demo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.hanyi.demo.service;
+
+import com.hanyi.demo.entity.Employee;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author hanyi
+ * @since 2019-11-09
+ */
+public interface EmployeeService extends IService<Employee> {
+
+}
