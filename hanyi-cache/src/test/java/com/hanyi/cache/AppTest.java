8173b1397ec2f5475a913124abb138ae2ba91cc3
Dearker <Stellar789>
2020-12-17 16:39:53 +0800

    提交redis存储对象自动过滤字段为null的方法

--- hanyi-cache/src/test/java/com/hanyi/cache/AppTest.java
+++ hanyi-cache/src/test/java/com/hanyi/cache/AppTest.java
@@ -1,5 +1,7 @@
 package com.hanyi.cache;
 
+import com.alibaba.fastjson.JSON;
+import com.hanyi.cache.entity.Book;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,20 +30,35 @@ public class AppTest {
     private StringRedisTemplate stringRedisTemplate;
 
     @Resource
-    private RedisTemplate<Object,Object> redisTemplate;
+    private RedisTemplate<Object, Object> redisTemplate;
 
+    /**
+     * 使用stringRedisTemplate
+     */
     @Test
-    public void redisTest(){
-        stringRedisTemplate.opsForHash().put("1","21","323");
+    public void redisTest() {
+        stringRedisTemplate.opsForHash().put("1", "21", "323");
         Object o = stringRedisTemplate.opsForHash().get("1", "21");
         System.out.println(o);
     }
 
+    /**
+     * 使用redisTemplate
+     */
     @Test
-    public void hashTest(){
-        redisTemplate.opsForHash().put("2","32","13");
+    public void hashTest() {
+        redisTemplate.opsForHash().put("2", "32", "13");
         Object o = redisTemplate.opsForHash().get("2", "32");
         System.out.println(o);
+    }
+
+    /**
+     * redis会自动过滤掉属性为null的字段，存储进去的实际上只有name的值
+     */
+    @Test
+    public void stringTest() {
+        Book book = new Book(null, "柯基", null);
+        stringRedisTemplate.opsForHash().put("1", "哈哈哈", JSON.toJSONString(book));
     }
 
 }
