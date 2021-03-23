c6fd7227e89e089d9a77442edb4c03842da91b7d
Dearker <1823268262@qq.com>
2021-03-23 21:30:05 +0800

    添加stream实体类的构造方法

--- hanyi-daily/src/main/java/com/hanyi/daily/pojo/Student.java
+++ hanyi-daily/src/main/java/com/hanyi/daily/pojo/Student.java
@@ -29,4 +29,9 @@ public class Student {
 
     private LocalDateTime createTime;
 
+    public Student(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 }
